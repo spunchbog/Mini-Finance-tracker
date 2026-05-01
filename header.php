fb69861f2c59cfe112897f6d7f1f44bd1e1dd43f
rampubalan-bit <rampubalan@gmail.com>
2026-05-01 18:49:51 +0800

    Add header.php for session management and navigation

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+session_start(); // Important: Allows the script to access $_SESSION data
+$deadline = "2026-10-10 23:59:59"; 
+?>
+
+<h1 align='center'>FinTrack - Personal Finance & Expense Tracker</h1>
+<hr>
+
+<?php if (!empty($_SESSION['role'])) { ?>
+    <?php if ($_SESSION['role'] == "admin") { ?>
+        | <a href='index.php'>Homepage</a>
+        | <a href='user-management.php'>User Management</a>
+        | <a href='category.php'>Category Manager</a>
+    <?php } elseif ($_SESSION['role'] == "user") { ?>
+        | <a href='index.php'>Homepage</a>
+        | <a href='dashboard.php'>Dashboard</a>
+        | <a href='transaction.php'>Transactions</a>
+        | <a href='budget.php'>Budgets</a>
+        | <a href='reports.php'>Reports</a>
+    <?php } ?>
+    | <a href='logout.php'>Logout</a>
+<?php } else { ?>
+    | <a href='index.php'>Home</a>
+    | <a href='login.php'>Login</a>
+    | <a href='signup.php'>Sign Up</a>
+<?php } ?>
+<hr>
