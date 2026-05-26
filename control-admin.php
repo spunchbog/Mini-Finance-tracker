7de250c35f9da63320dc41ecee8f63a75616ee2e
rampubalan-bit <rampubalan@gmail.com>
2026-05-27 01:57:05 +0800

    Delete control-admin.php

--- control-admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-#to allow admin only to access the file
-
-if(empty($_SESSION['role']) || $_SESSION['role'] != "admin"){
-    die("<script>alert('please login');
-    window.location.href='logout.php';</script>")
-}
-
-?>
