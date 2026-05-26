6fe79a6d449e36d9bd867ad4d87d30d41f360202
rampubalan-bit <rampubalan@gmail.com>
2026-05-27 01:57:28 +0800

    Delete control-user.php

--- control-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-#to allow user that register only to access the file
-
-if(empty($_SESSION['role']) || $_SESSION['role'] != "user"){
-    die("<script>alert('please login');
-    window.location.href='logout.php';</script>")
-}
-
-?>
