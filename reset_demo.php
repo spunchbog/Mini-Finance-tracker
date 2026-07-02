41777ab459fab6ed1fd8da36104d988b1a687977
BuildTools <unconfigured@null.spigotmc.org>
2026-07-02 14:40:00 +0800

    remove development file reset_demo.php

--- reset_demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('db_connect.php');
-// Reset User 1 to "New User" status
-mysqli_query($conn, "UPDATE user SET setup_complete = 0 WHERE user_id = 1");
-header("Location: signup.php");
-?>
