04f13b996a80901edd9486c483a63b992a1738a7
rampubalan-bit <rampubalan@gmail.com>
2026-07-02 14:39:45 +0800

    Delete reset_demo.php

--- reset_demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('db_connect.php');
-// Reset User 1 to "New User" status
-mysqli_query($conn, "UPDATE user SET setup_complete = 0, initial_capital = 0 WHERE user_id = 1");
-header("Location: InitialPage.php");
-?>
